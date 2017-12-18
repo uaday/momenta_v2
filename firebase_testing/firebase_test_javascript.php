bcc0bb6b72c6874aa374523ad02fa57af9b78322
uaday <uadayghosh@gmail.com>
2017-12-18 17:57:42 +0600

    firebase_add

---
+++ firebase_testing/firebase_test_javascript.php
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+
+   URL: https://gcm-http.googleapis.com/gcm/send 
+   HEADER:
+    Content-Type:application/json
+    Authorization:key=AAAAbk9Sb94:APA91bFmrdXbcxMkiD8o5LdHgY2aO9auqJpe0fwlIardXATHYYeGfeYIrS-RQbQ63EtrVlLql2KCI6_rav5jKh3kiZsht1Ev1u1BfH7fbo8srg_NbPAsQyx4NiFwuaWw25TQcF4acE8T
+
+   Body : 
+     {
+      "to": "/topics/Dhaka",
+       "data": { "message": "This is a GCM Topic Message!",
+        }
+</script>
